b8c7ee936e255c0e7e7b6cc70710ef521d739aa7
Abdurrehman4455 <rajamani456@outlook.com>
2024-07-17 21:28:11 +0500

    ali

--- src/Compoments/Button.jsx
+++ src/Compoments/Button.jsx
@@ -3,7 +3,7 @@ import Right from'../Icon/Vector.png';
 
 const Button = ({label,image}) => {
   return (
-   <button className='px-5  py-3  mt-[15%] bg-[#4CAF4F] text-[#FFFFFF] flex ml-10   max-sm:text-center max-sm:text-[13px] max-sm:py-2 rounded-lg  text-center' >
+   <button className='px-5  py-3  mt-[15%] bg-[#4CAF4F] text-[#FFFFFF] flex ml-10   max-sm:text-center max-sm:text-[13px] max-sm:py-2 rounded-lg  text-center hover:bg-[#3E8E41]' >
      
       <p className='text-center  max-sm:mt-2'>{label}</p>
       <div className=' mt-[11px] ml-3 max-sm:mt-4'>
--- src/Compoments/Button2.jsx
+++ src/Compoments/Button2.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button2 = () => {
   return (
     <div>
-      <button className='bg-[#4CAF4F] text-[#FFFFFF] px-20 py-4 rounded-lg max-sm:text-[12px] max-sm:px-4' >Register</button>
+      <button className='bg-[#4CAF4F] text-[#FFFFFF] px-20 py-4 rounded-lg max-sm:text-[12px] max-sm:px-4 hover:bg-[#3E8E41]' >Register</button>
     </div>
   )
 }
