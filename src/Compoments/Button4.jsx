fba8434d9c9c31698232c49dc7be1745a42c322e
Abdurrehman4455 <rajamani456@outlook.com>
2024-07-17 21:24:43 +0500

    changes

--- src/Compoments/Button4.jsx
+++ src/Compoments/Button4.jsx
@@ -4,7 +4,7 @@ const Button4 = ({name}) => {
   return (
     <div className='flex justify-center'>
     <div  className='flex items-center'>
-        <button className='mt-10  bg-[#4CAF4F] text-[#FFFFFF] px-8 py-4 rounded-lg flex max-sm:text-[20px]'>
+        <button className='mt-10  bg-[#4CAF4F] text-[#FFFFFF] px-8 py-4 rounded-lg flex max-sm:text-[20px] hover:bg-[#3E8E41]'>
 
              {name}
             <img  src={Right}  className='ml-2 mt-[10px]' alt=''></img>
