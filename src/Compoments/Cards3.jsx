aa1c3e5af579fffc91c1e48d9bd25b50204a11e5
Abdurrehman4455 <rajamani456@outlook.com>
2024-07-18 21:52:25 +0500

    chnages

--- src/Compoments/Cards3.jsx
+++ src/Compoments/Cards3.jsx
@@ -3,10 +3,10 @@ import imgs from'../Icon/Right.png'
 
 const Cards3 = () => {
   return (
-    <div className='pt-10 ml-[12%] max-sm:flex-col max-sm:mr-10 max-sm: flex-wrap max-sm:align-strech max-sm: justify-center  max-sm:align-baseline space-x-2 ' >
+    <div className='pt-10 ml-[12%] max-sm:flex-col max-sm:mr-10 max-sm: flex-wrap max-sm:align-strech max-sm: justify-center  max-sm:align-baseline space-x-2 md:mr-[30%] ' >
 
          <div className='ml-[15%]   w-full flex justify-center  max-sm:flex-col max-sm:mt-5 max-sm:items-center'>
-        <div className='w-[15%] h-[290px] bg-card-bg bg-no-repeat     rounded-lg  max-sm:w-[60%] z-10  max-sm:mr-[64px] '>
+        <div className='w-[15%] h-[290px] bg-card-bg bg-no-repeat     rounded-lg  max-sm:w-[60%] z-10  max-sm:mr-[64px] md:w-[40%] '>
           <div className='flex justify-center'>
            <div className='w-[240px] rounded-lg bg-white pt-10 mt-[60%] h-[150px] mr-[10%] text-center  max-sm:ml-5 border-2 border-grey-500  max-sm:text-[10px] ml-4 '>
             <p className='text-[#717171] font-bold'>Creating Streamlined Safeguarding Processes with OneRen</p>
@@ -18,7 +18,7 @@ const Cards3 = () => {
            </div>
           </div>
         </div>
-        <div className='w-[15%] h-[290px] bg-cards-bg  bg-no-repeat   rounded-lg max-sm:mt-[15%]  max-sm:w-[60%]  max-sm:mr-[90px] ml-4 ' >
+        <div className='w-[15%] h-[290px] bg-cards-bg  bg-no-repeat   rounded-lg max-sm:mt-[15%]  max-sm:w-[60%]  max-sm:mr-[90px] ml-4 md:w-[40%] ' >
              <div className='flex justify-center'>
              <div className='w-[240px] rounded-lg bg-white pt-10 mt-[60%] h-[150px] mr-[10%] text-center  max-sm:ml-5 border-2 border-grey-500  max-sm:text-[10px]'>
               <p className='text-[#717171] font-bold'>What are your safeguarding responsibilities and how can you manage them?</p>
@@ -32,7 +32,7 @@ const Cards3 = () => {
 
             
         </div>
-        <div className='w-[20%] h-[290px] bg-cards-bg1 bg-no-repeat    rounded-lg  max-sm:mt-4 max-sm:w-[60%] items-center justify-center  max-sm:mr-[90px] ml-4 '>
+        <div className='w-[20%] h-[290px] bg-cards-bg1 bg-no-repeat    rounded-lg  max-sm:mt-4 max-sm:w-[60%] items-center justify-center  max-sm:mr-[90px] ml-4 md:w-[40%]'>
              
         <div className='flex justify-center'>
            
@@ -51,23 +51,7 @@ const Cards3 = () => {
         </div>
 
         
-        <div className='w-[20%] h-[290px] bg-cards-bg1 bg-no-repeat  mr-[50%]      rounded-lg  max-sm:mt-4 max-sm:w-[60%] items-center justify-center max-sm:mr-[90px] ml-4 '>
-             
-             <div className='flex justify-center '>
-               
-             <div className='w-[240px] rounded-lg bg-white pt-10 mt-[60%] h-[150px] mr-[10%] text-center  max-sm:ml-5 border-2 border-grey-500  max-sm:text-[10px]'>
-                   <p className='text-[#717171] font-bold'>Revamping the Membership Model with Triathlon Australia</p>
-                   <div className='flex ml-20 mt-2'>
-                   <span>Read more</span>
-     
-                   <img src={imgs} alt='aa'className='ml-3'></img>
-                   </div>
-                   </div>
-                  </div>
-                  
-     
-               
-             </div>
+        
 
         
             
--- src/Compoments/Nav.jsx
+++ src/Compoments/Nav.jsx
@@ -19,7 +19,7 @@ const Nav = () => {
     
   return ( 
     <div className="w-full bg-[#FFFFFF] flex flex-wrap justify-between items-center ml-10  p-1 sm:flex-nowrap sm:ml-0 sm:mt-0 overflow-x-hiddden max-sm:overflow-x-hidden max-sm:ml-0 md:flex-nowrap md:ml-0">
-    <div className=" w-[30%]  max-sm:w-[20%] flex items-center font-semibold  sm:pl-[10%] max-sm:h-[30px] max-sm:mt-5 max-sm:overflow=x-hidden max-sm:ml-10 md:mt-10 ">
+    <div className=" w-[30%]  max-sm:w-[20%] flex items-center font-semibold  sm:pl-[10%] max-sm:h-[30px] max-sm:mt-5 max-sm:overflow=x-hidden max-sm:ml-10 md:mt-10 lg:mt-0 ">
         <img 
             src={Logo} 
             alt="logo" 
@@ -28,7 +28,7 @@ const Nav = () => {
         <span className="ml-2 font-bold text-lg sm:text-4xl leading-9 text-[#263238] max-sm:text-[27px]">Nexcent</span>
     </div>
 
-        <div className="w-[50%] max-sm:w-[50%] flex  max-sm:flex-row max-sm:items-center mt-2 sm:mt-0 max-sm:overflow-hidden md:justify-end md:mr-4  pr-11 lg:mr-4 max-sm:justify-end sm:justify-end sm:mt-7">
+        <div className="w-[50%] max-sm:w-[50%] flex  max-sm:flex-row max-sm:items-center mt-2 sm:mt-0 max-sm:overflow-hidden md:justify-end md:mr-4  pr-11 lg:mr-4 max-sm:justify-end sm:justify-end sm:mt-7 lg:mt-0">
         
         <div className="md:block lg:hidden max-sm:block max-md:hidden sm:block md:mt-10  max-sm:mt-5 max-sm:ml-[13px] sm:justify-end">
           <button onClick={toggleDropdown} className="text-lg font-semibold flex items-cente max-sm:ml-">
