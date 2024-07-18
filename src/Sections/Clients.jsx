017b6f9b1e48de4b18f98ef0e3e7b65f129ffd28
Abdurrehman4455 <rajamani456@outlook.com>
2024-07-18 14:24:38 +0500

    changes

--- src/Sections/Clients.jsx
+++ src/Sections/Clients.jsx
@@ -11,48 +11,37 @@ import Logo7 from '../Images/Logo (10).png'
 
 const Clients = () => {
   return (
-    <div >
-         <div className=''>
-          <h1 className='text-center text-[#4D4D4D] font-semibold text-[50px] mt-4 max-sm:text-[20px]'>Our Clients</h1>
-        <p className='text-[#717171] text-center text-[19px] mt-2 max-sm:text-[15px]'>we have  been working with some Fortune 500+clients </p>
-
-        <div className='pt-10 flex  pl-[10px] w-full justify-around  max-sm:flex  max-sm:items-center '>
-
-            <div className=''>
-                   <img src={Logo1} alt='logo' className='max-sm:ml-2' ></img>
-                
-            </div>
-           <div className=' pl-[15px]'>
-                  <img src={Logo2} alt='logo' ></img>
-            </div>
-            <div className=' pl-[15px]'>
-            <img src={Logo3} alt='logo' ></img>
-                
-            </div>
-            <div className=' pl-[15px]'>
-            <img src={Logo4} alt='logo' ></img>
-            </div>
-            <div className=' pl-[15px]'>
-            <img src={Logo5} alt='logo' ></img>
-            </div>
-
-            <div className=' pl-[15px]'>
-            <img src={Logo6} alt='logo' ></img>
-                
-            </div>
-            <div className=' pl-[15px]'>
-            <img src={Logo7} alt='logo' ></img>
-                
-            </div>
-
-            
-
-
+    <div>
+    <div className=''>
+      <h1 className='text-center text-[#4D4D4D] font-semibold text-[50px] mt-4 max-sm:text-[20px] md:text-[40px] md:mt-6'>Our Clients</h1>
+      <p className='text-[#717171] text-center text-[19px] mt-2 max-sm:text-[15px] md:text-[18px] md:mt-4'>We have been working with some Fortune 500+ clients</p>
+  
+      <div className='pt-10 flex pl-[10px] w-full justify-around max-sm:flex max-sm:items-center md:pl-0 md:justify-between md:pt-8 md:ml-3'>
+        <div className='md:w-[12%]'>
+          <img src={Logo1} alt='logo' className='max-sm:ml-2 md:ml-0 md:w-[50%]' />
         </div>
-      
-           
-         </div>
+        <div className='pl-[15px] md:pl-0 md:w-[12%]'>
+          <img src={Logo2} alt='logo' className='md:w-[55%]' />
+        </div>
+        <div className='pl-[15px] md:pl-0 md:w-[12%]'>
+          <img src={Logo3} alt='logo' className='md:w-[50%]' />
+        </div>
+        <div className='pl-[15px] md:pl-0 md:w-[12%]'>
+          <img src={Logo4} alt='logo' className='md:w-[50%]' />
+        </div>
+        <div className='pl-[15px] md:pl-0 md:w-[12%]'>
+          <img src={Logo5} alt='logo' className='md:w-[50%]' />
+        </div>
+        <div className='pl-[15px] md:pl-0 md:w-[12%]'>
+          <img src={Logo6} alt='logo' className='md:w-[50%]' />
+        </div>
+        <div className='pl-[15px] md:pl-0 md:w-[12%]'>
+          <img src={Logo7} alt='logo' className='md:w-[50%]' />
+        </div>
+      </div>
     </div>
+  </div>
+  
   )
 }
 
--- src/Sections/Community.jsx
+++ src/Sections/Community.jsx
@@ -4,18 +4,24 @@ import Icon from'../Icon/Icon (5).png'
 
 const Community = () => {
   return (
-    <div>  <h1 className='text-center text-[#4D4D4D] font-semibold text-[50px] mt-4 max-sm:text-[20px]'>  Manage your entire community <br/>in a single system  </h1>
-    <p className='text-[#717171] text-center text-[19px] mt-2'>Who is Nextcent suitable for?
-    </p>
+    <div>
+  <h1 className='text-center text-[#4D4D4D] font-semibold text-[50px] mt-4 max-sm:text-[20px] md:text-[40px] md:mt-6'>
+    Manage your entire community <br />in a single system
+  </h1>
+  <p className='text-[#717171] text-center text-[19px] mt-2 max-sm:text-[15px] md:text-[18px] md:mt-4'>
+    Who is Nextcent suitable for?
+  </p>
 
-    <Cards className='pt-[15px]'
-    
+  <Cards className='pt-[15px] md:pt-[30px]'
     Icon={Icon}
-    content='Membership'content2='Organisations' content3='National' content4='Assosciations' content5='Clubs And' content6='Groups'
-    
-    ></Cards>
-    
-    </div>
+    content='Membership' 
+    content2='Organisations' 
+    content3='National' 
+    content4='Associations' 
+    content5='Clubs And' 
+    content6='Groups'
+  />
+</div>
   )
 }
 
