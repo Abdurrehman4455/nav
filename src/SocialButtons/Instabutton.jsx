611731f41a466d4b6f6d2d79ae296f463cad6c44
Abdurrehman4455 <rajamani456@outlook.com>
2024-07-20 01:03:44 +0500

    aaa

--- src/SocialButtons/Instabutton.jsx
+++ src/SocialButtons/Instabutton.jsx
@@ -3,7 +3,12 @@ import Button from'../SocialButtons/Icon/Path.png';
 import basket from'../SocialButtons/Icon/basket.png'
 import Twit from'../SocialButtons/Icon/twiter.png';
 import youtube from'../SocialButtons/Icon/youtube.png';
+import path from'../Images/Path.svg'
+import path1 from'../Images/Path12.svg'
+import path2 from'../Images/Path22.svg'
+import path3 from'../Images/66.svg'
 import { ImPlay } from "react-icons/im";
+
 const Instabutton = ({icon,name}) => {
 
 
@@ -22,16 +27,16 @@ const Instabutton = ({icon,name}) => {
   return (
     <div>
        <button className='w-12 h-12 rounded-full bg-[grey]'>
-           <img src={Button}  alt='a' className='w-5 ml-3' onClick={handleButtonClick}  ></img>
+           <img src={path}  alt='a' className='w-5 ml-3' onClick={handleButtonClick}  ></img>
        </button>
        <button className='w-12 h-12 rounded-full bg-[grey] ml-2' onClick={handleButtonClick2}>
-           <img src={basket} alt='a' className='w-5 ml-3'></img>
+           <img src={path1} alt='a' className='w-5 ml-3'></img>
        </button>
        <button className='w-12 h-12 rounded-full bg-[grey] ml-2' onClick={handleButtonClick3}>
-           <img src={Twit} alt='a'className='w-5 ml-3' ></img>
+           <img src={path2} alt='a'className='w-5 ml-3' ></img>
        </button>
        <button className='w-12 h-12 rounded-full bg-[grey] ml-2' onClick={handleButtonClick4}>
-         <ImPlay className='w-5 ml-3' />
+       <img src={path3} alt='a'className='w-5 ml-3' ></img>
        </button>
     </div>
   )
