d9747baadf9680e6fe7e8436260110b2508e403e
Abdurrehman4455 <rajamani456@outlook.com>
2024-07-23 19:07:59 +0500

    ali

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
   return ( 
   
     <div className='w-full bg-[#F5F7FA] h-[700px] flex justify-around flex-wrap max-sm:flex-col max-sm:items-center pr-5  md:justify-around md:h-[700px] lg:justify-around lg:h-[700px] lg:mt-0 lg:w-[100%]'>
-    <div className="w-full md:w-[40%] lg:w-[30%] pt-[10%] md:pt-[10%] lg:pt-[10%] max-sm:pt-2 max-sm:w-full max-sm:pl-0 max-sm:pr-4  md:justify-center items-center  lg:ml-[6%]  ">
+    <div className="w-full md:w-[40%] lg:w-[30%] pt-[10%] md:pt-[10%] lg:pt-[10%] max-sm:pt-2 max-sm:w-full max-sm:pl-0 max-sm:pr-4  md:justify-center items-center  lg:ml-[6%] md:pl-[10%] lg:pl-0 ">
       <h1 className="font-semibold text-[55px] leading-[76px] text-[#4D4D4D] max-sm:text-left max-sm:text-[20px] max-sm:leading-[24px] max-sm:mt-1 md:text-[40px] md:leading-[56px] md:mt-2 lg:text-[48px] lg:leading-[66px] lg:mt-3 max-sm:text-[28px] sm:text-[30px]">
         Lesson and insights
       </h1>
