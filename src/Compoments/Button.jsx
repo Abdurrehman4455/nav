590d4ec5d575c1e5fa69843ff2b4249db16f416d
Abdurrehman4455 <rajamani456@outlook.com>
2024-07-24 20:25:21 +0500

    ali

--- src/Compoments/Button.jsx
+++ src/Compoments/Button.jsx
@@ -4,7 +4,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Button = ({label,image}) => {
   return (
-   <button className='px-2 py-2 mt-7 bg-[#4CAF4F] text-[#FFFFFF] flex   max-sm:text-center max-sm:text-[13px] max-sm:py-2 rounded-lg  text-center   md:hiiden lg:block lg:flex  '  >
+   <button className='px-2 py-2 mt-7 bg-[#4CAF4F] text-[#FFFFFF] flex   max-sm:text-center max-sm:text-[13px] max-sm:py-2 rounded-lg  text-center   md:hiiden lg:block lg:flex lg:mt-[25%]  '  >
      
       <p className='text-center  max-sm:mt-2'>{label}</p>
       <div className=' mt-[5px] ml-3 max-sm:mt-3 '>
--- src/Compoments/Cards.jsx
+++ src/Compoments/Cards.jsx
@@ -11,23 +11,23 @@ import { FaRegHandSpock } from "react-icons/fa";
 const Cards = ({content,content2,content3,content4,content5,content6}) => {
   return (
   
-    <div className="flex flex-col items-center space-y-6 pt-10 sm:flex-row sm:justify-around sm:space-x-4 sm:space-y-0 sm:pt-6 md:space-x-6 md:pt-8 lg:space-x-8 lg:pt-10">
-  <div className="w-[45%] h-[270px] bg-white rounded-lg border border-gray-300 shadow-lg flex justify-center items-center sm:w-[25%] sm:h-[300px] md:w-[30%] md:h-[320px]  lg:h-[350px] max-sm:w-[69%] ">
+    <div className="flex flex-col items-center space-y-6 pt-10 sm:flex-row sm:justify-around sm:space-x-4 sm:space-y-0 sm:pt-6 md:space-x-6 md:pt-8 lg:space-x-8 lg:pt-10 p-[4%]">
+  <div className="w-[45%] h-[270px] bg-white rounded-lg border border-gray-300 shadow-lg flex justify-center items-center sm:w-[25%] sm:h-[300px] md:w-[30%] md:h-[320px]  lg:h-[350px] max-sm:w-[69%] p-[4%] ">
     <div className="flex flex-col items-center">
-      <div>
+      <div className='mb-6'>
         <VscOrganization alt="icon" className="w-14 md:w-16" width={900}/>
       </div>
       <div>
         <h1 className="text-[#4D4D4D] font-bold text-2xl sm:text-xl md:text-2xl lg:text-3xl max-sm:ml-2 lg:ml-2">{content}</h1>
         <span className="text-[#4D4D4D] font-bold text-2xl sm:text-xl md:text-2xl lg:text-3xl max-sm:ml-2 lg:ml-2">{content2}</span>
       </div>
-      <div>
-        <p className="text-center text-[#717171] text-sm md:text-base lg:text-lg">Our membership management<br />software provides full automation of<br />membership renewals and payments</p>
+      <div className='p-[6%]'>
+        <p className=" text-center text-[#717171] text-sm md:text-base lg:text-lg">Our membership management<br />software provides full automation of<br />membership renewals and payments</p>
       </div>
     </div>
   </div>
 
-  <div className="lg:w-[30%] h-[270px] bg-white rounded-lg border border-gray-300 shadow-lg flex justify-center items-center sm:w-[25%] sm:h-[300px] md:w-[30%] md:h-[320px]  lg:h-[350px] max-sm:w-[69%]">
+  <div className="lg:w-[30%] h-[270px] bg-white rounded-lg border border-gray-300 shadow-lg flex justify-center items-center sm:w-[25%] sm:h-[300px] md:w-[30%] md:h-[320px]  lg:h-[350px] max-sm:w-[69%] p-[4%]">
     <div className="flex flex-col items-center">
       <div>
         <LuBuilding  alt="icon" className="w-14 md:w-16 lg:w-20" /><br />
@@ -36,23 +36,23 @@ const Cards = ({content,content2,content3,content4,content5,content6}) => {
         <h1 className="text-[#4D4D4D] font-bold text-2xl sm:text-xl md:text-2xl lg:text-3xl max-sm:ml-10 lg:ml-10">{content3}</h1>
         <span className="text-[#4D4D4D] font-bold text-2xl sm:text-xl md:text-2xl lg:text-3xl max-sm:ml-5 lg:ml-4">{content4}</span>
       </div>
-      <div>
+      <div className='p-[6%]'>
         <p className="text-center text-[#717171] text-sm md:text-base lg:text-lg">Our membership management<br />software provides full automation of<br />membership renewals and payments</p>
       </div>
     </div>
   </div>
 
-  <div className="w-[60%]  lg:w-[30%]  h-[270px] bg-white rounded-lg border border-gray-300 shadow-lg flex justify-center items-center sm:w-[25%] sm:h-[300px] md:w-[30%] md:h-[320px] lg:w-[18%] lg:h-[350px] max-sm:w-[69%]">
+  <div className="w-[60%]  lg:w-[30%]  h-[270px] bg-white rounded-lg border border-gray-300 shadow-lg flex justify-center items-center sm:w-[25%] sm:h-[300px] md:w-[30%] md:h-[320px] lg:w-[18%] lg:h-[350px] max-sm:w-[69%] p-[4%]">
     <div className="flex flex-col items-center">
-      <div>
-        <FaRegHandSpock alt="icon" className="w-14 md:w-16 lg:w-20" /><br />
+      <div className=''>
+        <FaRegHandSpock alt="icon" className=" md:w-16 lg:w-20" /><br />
       </div>
       <div>
         <h1 className="text-[#4D4D4D] font-bold text-2xl sm:text-xl md:text-2xl lg:text-3xl lg:ml-5">{content5}</h1>
         <span className="text-[#4D4D4D] font-bold text-2xl sm:text-xl md:text-2xl lg:text-3xl max-sm:ml-2 lg:ml-9">{content6}</span>
       </div>
-      <div>
-        <p className="text-center text-[#717171] text-sm md:text-base lg:text-lg">Our membership management<br />software provides full automation of<br />membership renewals and payments</p>
+      <div className=' p-[6%]'>
+        <p className=" text-center text-[#717171] text-sm md:text-base lg:text-lg">Our membership management<br />software provides full automation of<br />membership renewals and payments</p>
       </div>
     </div>
   </div>
